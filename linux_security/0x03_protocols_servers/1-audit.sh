090d4c3e91048602f1da3a83144767755c7b95af
Mughal Bilal <94051316+vikingsuited@users.noreply.github.com>
2026-05-13 19:29:02 +0400

    Delete linux_security/0x03_protocols_servers/1-audit.sh

--- linux_security/0x03_protocols_servers/1-audit.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# This script audits the sshd_config file for specific settings
-grep -E "^(Include|KbdInteractiveAuthentication|UsePAM|X11Forwarding|PrintMotd|AcceptEnv|Subsystem|PasswordAuthentication|PermitRootLogin|AuthorizedKeysFile|TCPKeepAlive)" /etc/ssh/sshd_config
