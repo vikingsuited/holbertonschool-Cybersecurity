6b2b6f984b565cfbc038ed032cd8f023cce91a1e
Mughal Bilal <94051316+vikingsuited@users.noreply.github.com>
2026-05-13 19:29:10 +0400

    Delete linux_security/0x03_protocols_servers/2-harden.sh

--- linux_security/0x03_protocols_servers/2-harden.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Disable root login and empty passwords
-sed -i 's/PermitRootLogin yes/PermitRootLogin no/' /etc/ssh/sshd_config
-sed -i 's/#PermitEmptyPasswords yes/PermitEmptyPasswords no/' /etc/ssh/sshd_config
-systemctl restart ssh
