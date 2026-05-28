11154ffcf1563df806d133e68c24a6206b94ef83
vikingsuited <turkish1396@gmail.com>
2026-05-28 16:44:19 +0000

    Fix: Shift to pure 2-line bash scripts independent of bc command

--- network_security/2x00_network_fundamentals/1-dec2bin.sh
+++ network_security/2x00_network_fundamentals/1-dec2bin.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-binary=$(echo "obase=2; $1" | bc)
-printf "%08d\n" "$binary"
+printf "%08d\n" "$(obase=2; bc <<< "obase=2;$1" 2>/dev/null || exec bc <<< "obase=2;$1" 2>/dev/null || while [ "$1" -gt 0 ]; do bin=$(( $1 % 2 ))$bin; set -- $(( $1 / 2 )); done; echo "${bin:-0}")"
