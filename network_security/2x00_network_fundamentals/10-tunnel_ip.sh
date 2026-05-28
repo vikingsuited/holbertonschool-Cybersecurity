7add406ee0162d5a883cd3e3a610bc958feec285
vikingsuited <turkish1396@gmail.com>
2026-05-28 16:51:40 +0000

    Fix: Deploy 10-tunnel_ip.sh to the correct tracked repository folder

--- network_security/2x00_network_fundamentals/10-tunnel_ip.sh
+++ network_security/2x00_network_fundamentals/10-tunnel_ip.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-ip addr show tun0 2>/dev/null | grep -oP "(?<=inet\s)\d+(\.\d+){3}" | tr -d "
-" | xargs printf "%s"
+ip addr show tun0 2>/dev/null | grep -oP '(?<=inet\s)\d+(\.\d+){3}' | tr -d '\n'
