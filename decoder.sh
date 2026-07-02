c845d42ea57fffeef0cb4f49f482f51ba4d4c76e
vikingsuited <turkish1396@gmail.com>
2026-07-02 15:08:09 +0000

    Update file structure and remove old decoder

--- decoder.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-encoded="$1"
-encoded="${encoded#\{xor\}}"
-decoded=$(echo "$encoded" | base64 --decode 2>/dev/null | \
-  perl -ne 'foreach $c (split //) { printf "%s", chr(ord($c) ^ 0x5f) }')
-echo "$decoded"
