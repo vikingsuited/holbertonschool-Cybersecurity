098333fe896793ae222425e1975f43ad78ca61bc
Mughal Bilal <94051316+vikingsuited@users.noreply.github.com>
2026-08-16 16:14:26 +0400

    Update 0-attack_ip.sh

--- web_application_security/0x0b_web_application_fast_incident_response/0-attack_ip.sh
+++ web_application_security/0x0b_web_application_fast_incident_response/0-attack_ip.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-
-file=logs.txt
-grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" "$file" | sort | uniq -c | sort -rn | head -1 | awk '{print $2}'
+cat logs.txt | cut -d' ' -f1 | grep '[.]' | sort | uniq -c | tail -1 | cut -d' ' -f5
